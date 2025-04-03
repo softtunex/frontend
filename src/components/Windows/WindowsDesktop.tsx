125c9679906083390ac3fadd401ae210bdc81cb9
Buari Olatunji <buariolatunji@gmail.com>
2025-04-03 20:21:35 +0100

    fix issues

--- src/components/Windows/WindowsDesktop.tsx
+++ src/components/Windows/WindowsDesktop.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import React, { useState, useEffect } from "react";
 import { Icon } from "@iconify/react";
 import "./Windows.css";
 import Window from "../WindowModal/Window";
